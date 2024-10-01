3c0f733af3e1c1d9032e12a2c0314c5e3ee4b6ba
JaysonTF <figueroaj102289@gmail.com>
2024-10-01 10:43:23 +0800

    fix spacing in pdf

--- src/components/DownloadDetails.jsx
+++ src/components/DownloadDetails.jsx
@@ -77,19 +77,19 @@ const HtmlToPdf = ({ property }) => {
                 ref={printRef}
                 className={`px-2 bg-white absolute top-[-9999px] w-[210mm] box-border ${isContentVisible ? '' : 'hidden'}`}
             >
-                <div className="grid grid-cols-2 gap-5 p-5">
+                <div className="grid grid-cols-2 gap-2 p-2">
                     <div className="flex flex-col col-span-2 justify-center items-center">
                         <h2 className="text-xl font-semibold uppercase">{property.name}</h2>
                         <p className="text-sm font-thin">{property.location}</p>
                     </div>
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col font-light">
                         <div className="py-0 my-0 flex text-[11px]">
-                            <span className="font-bold px-1">Location:</span>
+                            <span className="px-1">Location:</span>
                             <span className="px-1">{property.location}</span>
                         </div>
                         <div className="py-0 my-0 flex text-[11px]">
-                            <span className="font-bold px-1">Price:</span>
+                            <span className="px-1">Price:</span>
                             <span className="px-1">
                                 {new Intl.NumberFormat('en-PH', {
                                     style: 'currency',
@@ -98,50 +98,50 @@ const HtmlToPdf = ({ property }) => {
                             </span>
                         </div>
                         <div className="py-0 my-0 flex text-[11px]">
-                            <span className="font-bold px-1">Floor Area:</span>
-                            <span className="px-1">{property.floorArea} sqm</span>
+                            <span className="px-1">Floor Area:</span>
+                            <span className="px-1">{property.floorArea} Sqm</span>
                         </div>
                         <div className="py-0 my-0 flex text-[11px]">
-                            <span className="font-bold px-1">Lot Area:</span>
-                            <span className="px-1">{property.lotArea} sqm</span>
+                            <span className="px-1">Lot Area:</span>
+                            <span className="px-1">{property.lotArea} Sqm</span>
                         </div>
                     </div>
 
-                    <div className="flex flex-col">
+                    <div className="flex flex-col font-light">
                         <div className="flex text-[11px] py-0 my-0">
-                            <span className="font-bold px-1">Direct Sales Marketing Assistant:</span>
+                            <span className="px-1">Direct Sales Marketing Assistant:</span>
                             <span className="px-1">Carla Flores</span>
                         </div>
                         <div className="flex text-[11px] py-0 my-0">
-                            <span className="font-bold px-1">PRC Licensed Number:</span>
-                            <span className="px-1">0016602</span>
+                            <span className=" px-1">PRC Licensed Number:</span>
+                            <span className="px-1 ">0016602</span>
                         </div>
                         <div className="flex text-[11px] py-0 my-0">
-                            <span className="font-bold px-1">Contact #:</span>
+                            <span className=" px-1">Contact #:</span>
                             <span className="px-1">0961-279-6675</span>
                         </div>
                         <div className="flex text-[11px] py-0 my-0">
-                            <span className="font-bold px-1">Email:</span>
+                            <span className=" px-1">Email:</span>
                             <span className="px-1">florescarla399@gmail.com</span>
                         </div>
                     </div>
 
-                    <div className="flex flex-col col-span-2">
+                    <div className="flex flex-col col-span-2 font-light">
                         <div className="flex text-[11px] py-0 my-0 border border-black mb-1">
-                            <span className="font-bold px-1">Features:</span>
+                            <span className="px-1">Features:</span>
                             <span className="px-1">{property.features.join(', ')}</span>
                         </div>
                         <div className="flex text-[11px] py-0 my-0 border border-black">
-                            <span className="font-bold px-1">Payment Options:</span>
+                            <span className="px-1">Payment Options:</span>
                             <span className="px-1">{property.paymentTerms.join(', ')}</span>
                         </div>
                     </div>
 
                     <div className="flex flex-col col-span-2">
                         <p className="text-xl font-bold uppercase">Images</p>
-                        <div className="grid grid-cols-3 gap-4 col-span-2">
+                        <div className="grid grid-cols-3 gap-1 col-span-2">
                             {property.images.slice(0, 15).map((image, index) => (
-                                <div key={index} className="p-2 w-full h-32">
+                                <div key={index} className="p-1 w-full h-32">
                                     <img
                                         src={image}
                                         alt={index} // Use a descriptive alt text
